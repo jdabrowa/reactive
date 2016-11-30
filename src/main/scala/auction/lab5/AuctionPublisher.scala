c6f1a5e4f00d4c9e97b697fa9e8341a511c13c9f
nachteil <nachteil@gmail.com>
2016-11-30 23:59:43 +0100

    Changes...

--- src/main/scala/auction/lab5/AuctionPublisher.scala
+++ src/main/scala/auction/lab5/AuctionPublisher.scala
@@ -7,9 +7,14 @@ import auction.lab5.Notifier.Notify
 
 class AuctionPublisher extends Actor {
 
+  log("Creating publisher")
+
   override def receive: Receive = {
     case Notify(auctionTitle, buyerName, currentPrice) => {
       log(s"$buyerName now leads auction $auctionTitle for $currentPrice")
+    }
+    case _ => {
+      log("Default handler")
     }
   }
 
